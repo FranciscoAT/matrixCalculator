78d897f254154a88375824c274fdc44e9033208f
FranciscoAT <francisco.ad.trindade@gmail.com>
2015-03-12 13:09:52 -0400

    Create new Matrix

--- matrixCalculator/src/matrixCalculator/Screen.java
+++ matrixCalculator/src/matrixCalculator/Screen.java
@@ -1,20 +1,23 @@
 package matrixCalculator;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class Screen extends JPanel {
+public class Screen extends JPanel implements ActionListener{
 	
-	private ArrayList matrixList;
+	private static int maxArrLength = 10;
+	private ArrayList<Matrix> matrixList;
 	private JLabel title;
 	private JPanel topPanel;
+	private JButton createMatrixAButton, createMatrixBButton;
 	
 	public Screen(){
+		matrixList = new ArrayList<Matrix>();
 		setSize(new Dimension(780,525));
 		
 		topPanel = new JPanel();
-		//topPanel.setPreferredSize(new Dimension(780, 50));
-		topPanel.setBackground(Color.CYAN);
 		
 		title = new JLabel("Hit \"Create\" to get started!");
 		title.setHorizontalTextPosition(SwingConstants.HORIZONTAL);
@@ -24,6 +27,25 @@ public class Screen extends JPanel {
 		topPanel.add(title);
 		add(topPanel, BorderLayout.NORTH);
 		System.out.println(this.getWidth());
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
+	public void add(Matrix m){
+		matrixList.add(m);
+	}
+	
+	public void createNewMatrixA(){
+		String[] maxLength = new String[maxArrLength];
+		for(int i = 0; i < maxLength.length; i++){
+			maxLength[i] = ""+(i+1);
+		}
+		
+		JComboBox numRows = new JComboBox(maxLength);
+		
+		add(numRows, BorderLayout.CENTER);
 	}
 	
 	
